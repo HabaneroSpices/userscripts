532c902f6c9e4e7966117cbf77199f3d61803af9
Chloe Spices <45343924+HabaneroSpices@users.noreply.github.com>
2023-07-24 21:54:03 +0200

    Update AB Hover on Cover.user.js

--- AB Hover on Cover/AB Hover on Cover.user.js
+++ AB Hover on Cover/AB Hover on Cover.user.js
@@ -1,1 +1,30 @@
-s
+// ==UserScript==
+// @name         AB Hover on Cover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  AB Hover on Cover!
+// @author       Habanero
+// @match        https://animebytes.tv/*
+// @icon         https://github.com/HabaneroSpices/userscripts/raw/main/img/steam.svg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  
+  var styles = `
+  .enlarge-onhover:hover {
+       width: auto;
+       height: 80%;
+  }
+  `
+  
+  var styleSheet = document.createElement("style")
+  styleSheet.innerText = styles
+  document.head.appendChild(styleSheet)
+  
+  var mainimg = document.querySelectorAll(".mainimg");
+  [].forEach.call(mainimg, function(mainimg) {
+    mainimg.querySelector('img').classList.add("enlarge-onhover");
+  });
+})();
