b4ab8cb296641838733833e1375dfcf685fc391e
Chloe Spices <45343924+HabaneroSpices@users.noreply.github.com>
2023-12-19 23:11:31 +0100

    Update Open In Steam.user.js

--- Open In Steam/Open In Steam.user.js
+++ Open In Steam/Open In Steam.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Open In Steam
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Replace "Store Page" button with "Open In Steam"!
 // @author       Habanero
 // @match        https://steamcommunity.com/*
 // @icon         https://github.com/HabaneroSpices/userscripts/raw/main/img/steam.svg
+// @downloadURL  https://github.com/HabaneroSpices/userscripts/raw/main/Open%20In%20Steam/Open%20In%20Steam.user.js
+// @updateURL    https://github.com/HabaneroSpices/userscripts/raw/main/Open%20In%20Steam/Open%20In%20Steam.user.js
 // @grant        none
 // ==/UserScript==
 
