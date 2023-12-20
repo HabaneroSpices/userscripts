44538a0e44a36ce2c8445481bb91910d8aa74f19
Chloe Spices <45343924+HabaneroSpices@users.noreply.github.com>
2023-12-20 19:24:20 +0100

    Update Fishtank Better Navigation.user.js

--- Fishtank Better Navigation/Fishtank Better Navigation.user.js
+++ Fishtank Better Navigation/Fishtank Better Navigation.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Fishtank Better Navigation
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.1
 // @description  Fishtank Better Navigation
 // @author       Credit some guy on le'chan
 // @match        https://www.fishtank.live/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fishtank.live
 // @downloadURL  https://github.com/HabaneroSpices/userscripts/raw/main/Fishtank%20Better%20Navigation/Fishtank%20Better%20Navigation.user.js
 // @updateURL    https://github.com/HabaneroSpices/userscripts/raw/main/Fishtank%20Better%20Navigation/Fishtank%20Better%20Navigation.user.js
+// @require     https://unpkg.com/keycode-js@3.1.0/dist/keycode.min.js
+// @require     https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
@@ -20,6 +22,68 @@
         if(document.querySelector('.secondary-panel_secondary-panel__vUc65')) {
             observer.disconnect();
 
+            /*** Auto Dismiss Seasonpass Toast ***/
+            const t0 = Date.now();
+            document.arrive(".Toast_close__c0JjL", function(controls) {
+                if ((Date.now() - t0)/1000 < 9) { //still allow using the GET SEASON PASS button
+                    document.querySelector('.Toast_close__c0JjL').click();
+                }
+            });
+
+            //capture keyboard input over the player
+            document.arrive(".LiveStreamsCloudflarePlayer_live-streams-player__OCZ2v", function(player) {
+                const overlay = document.createElement('div');
+                overlay.id = 'overlay';
+                overlay.style.position = 'absolute';
+                overlay.style.top = '0';
+                overlay.style.left = '0';
+                overlay.style.width = "inherit";
+                overlay.style.height = "inherit";
+                overlay.style.zIndex = '9999';
+                overlay.style.opacity = '0';
+                overlay.tabIndex = '0'; // Make the overlay focusable
+
+                player.appendChild(overlay);
+            });
+
+            //set thumbnails to HD (still right click on thumbnails in grid view)
+            document.arrive(".LiveStreamsGridItem_thumbnail__LAKyg",function(thumb) {
+                if (thumb.firstElementChild) {
+                    let src = thumb.firstElementChild.src;
+                    if (src.substring(src.length-4,src.length) !== "1080") {
+                        thumb.firstElementChild.src = src + "?height=1080";
+                    }
+                }
+            });
+
+            //define keyboard controls
+            const roomToKeys = [["1","NUMPAD1"],["2","NUMPAD2"],["3","NUMPAD3"],["4","NUMPAD4"],["5","NUMPAD5"],["6","NUMPAD6"],["7","NUMPAD7"],["8","NUMPAD8"],["9","NUMPAD9"],["0","NUMPAD0","MULTIPLY"],["DASH","SUBTRACT"]];
+            const keyCodeToRoom = {};
+            for (let n=0; n<roomToKeys.length; n++) {
+                const keys = roomToKeys[n];
+                for (let j=0; j<keys.length; j++) {
+                    keyCodeToRoom[ KeyCode["KEY_"+keys[j]] ] = n;
+                }
+            }
+
+            /*** Keyboard camera navigation (numbers + arrows) ***/
+            document.addEventListener("keydown", function(event) {
+                if (event.isComposing || document.activeElement?.selectionStart !== undefined || document.activeElement?.isContentEditable) {
+                    return;
+                }
+
+                if (keyCodeToRoom[event.keyCode] != null) {
+                    document.querySelector(".LiveStreamsListItem_live-streams-list-item__rALBj:nth-child("+(keyCodeToRoom[event.keyCode]+3)+")")?.click();
+                } else if (event.keyCode === KeyCode.KEY_UP) {
+                    document.querySelector(".LiveStreamsControls_prev-next__pbktS button")?.click();
+                } else if (event.keyCode === KeyCode.KEY_DOWN) {
+                    document.querySelector(".LiveStreamsControls_prev-next__pbktS button:nth-child(2)")?.click();
+                } else if (event.keyCode === KeyCode.KEY_M) {
+                    document.querySelector(".LiveStreamsControls_mute__POpUL")?.click();
+                }
+            });
+
+            // BETTER ROOM BETTER
             function createFishtankButton(text) {
                 var button = document.createElement('button');
                 button.classList.add('color-button_color-button__cW61T', 'color-button_md__GaczN');
